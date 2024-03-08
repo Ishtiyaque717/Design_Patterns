7e19e6eba8289a127ff7cfcc5ae8f256d9a2c8f5
Ishtiyaq717 <ishtiyaque@increasingly.com>
2024-03-08 18:10:31 +0530

    eager singleTone

---
+++ src/main/java/com/example/creationaldesginpattern/FactoryDesignPattern.java
@@ -0,0 +1,5 @@
+package com.example.creationaldesginpattern;
+
+public class FactoryDesignPattern {
+
+}
--- src/main/java/com/example/creationaldesginpattern/SingletoneDesginPattern.java
+++ src/main/java/com/example/creationaldesginpattern/SingletoneDesginPattern.java
@@ -2,36 +2,57 @@ package com.example.creationaldesginpattern;
 
 public class SingletoneDesginPattern {
 	public static void main(String[] args) {
-		Animal obj=Animal.getAnimal();
+		Animal obj = Animal.getAnimal();
 		System.out.println(obj.hashCode());
-		Animal obj2= Animal.getAnimal();
+		Animal obj2 = Animal.getAnimal();
 		System.out.println(obj2.hashCode());
-		
+
+		Jalebi obj3 = Jalebi.getJalebi();
+		Jalebi obj4 = Jalebi.getJalebi();
+		System.out.println(obj3.hashCode());
+		System.out.println(obj4.hashCode());
+
 	}
 }
 
+//---------------------------------------------------------------------------------------  //
+
+// Lazy SingleTon
 class Animal {
 	int eye;
 	int nose;
 	String name;
-	//---1 create Static instance variable of the class
+	// ---1 create Static instance variable of the class
 	private static Animal animal;
 
-	//---2 create private constructor
+	// ---2 create private constructor
 	private Animal(int eye, int nose, String name) {
 		this.eye = eye;
 		this.nose = nose;
 		this.name = name;
 	}
 
-	//---3 create static getinstanceMethod 
-	//---4 write a logic to check if object is already existing return the same object
+	// ---3 create static getinstanceMethod
+	// ---4 write a logic to check if object is already existing return the same
+	// object
 	public static Animal getAnimal() {
-		if (animal == null) {	
-			animal=new Animal(2, 1, "Dog");
+		if (animal == null) {
+			animal = new Animal(2, 1, "Dog");
 			return animal;
 		}
 		return animal;
 	}
+}
+// ---------------------------------------------------------------------------------------  //
+// Eager SingleTone
+class Jalebi {
+	private static Jalebi obj = new Jalebi();
 
+	private Jalebi() {
+
+	}
+
+	public static Jalebi getJalebi() {
+		return obj;
+	}
 }
